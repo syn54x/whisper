6862616bd15a601b75568dfd14ad3d62a4766ab8
Myg0t <myg0tm3dia@gmail.com>
2024-10-07 23:04:52 -0400

    add coverage badge

--- src/whisper/logging.py
+++ src/whisper/logging.py
@@ -1,13 +1,6 @@
 import logging
 from rich.logging import RichHandler
 
-# logging.basicConfig(
-#     level=logging.WARN,
-#     format="%(message)s",
-#     datefmt="[%X]",
-#     handlers=[RichHandler(rich_tracebacks=True, markup=True)],
-# )
-
 LOGGER = logging.getLogger("rich")
 
 LOGGER.setLevel(logging.WARN)

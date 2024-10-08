b6f2e59ed709aaaab85a253545ddc80e2bd3d803
Myg0t <myg0tm3dia@gmail.com>
2024-10-07 20:10:21 -0400

    update unit tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,14 +1,18 @@
 import pytest
 from typer.testing import CliRunner
-from whisper.config import app
+from rich.console import Console
+from unittest.mock import patch
+
+
+from whisper.cli.config import app
 
 runner = CliRunner()
 
 
 @pytest.fixture
 def mock_user_config(mocker):
-    mocker.patch("whisper.config.UserConfig.save")
-    return mocker.patch("whisper.config.UserConfig")
+    mocker.patch("whisper.cli.config.UserConfig.save")
+    return mocker.patch("whisper.cli.config.UserConfig")
 
 
 def test_set_command(mock_user_config):
@@ -30,3 +34,22 @@ def test_get_command(mock_user_config):
     mock_user_config.return_value.get_by_dot_notation.assert_called_once_with(
         "openai.api_key"
     )
+
+
+@pytest.fixture
+def mock_console():
+    with patch.object(Console, "print") as mock_print:
+        yield mock_print
+
+
+def test_themes_command(mock_console):
+    result = runner.invoke(app, ["themes"])
+    assert result.exit_code == 0
+    # Check if the print method was called at least once
+    assert mock_console.called
+    # You can further check for specific themes if you want to be very precise
+    args, kwargs = mock_console.call_args
+    print(dir(args))
+    output = str(args[0])
+    assert "solarized-dark" in output
+    assert "monokai" in output  # Assuming 'monokai' is a part of the themes
